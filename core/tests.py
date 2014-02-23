9bfd8c4a06dc09f186b5a8dc5b75defa81c8f590
Tetiana Pozniakova <tetiana.pozniakova@gmail.com>
2014-02-23 22:37:47 +0200

    Core unit test POC

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from core.models import *
+from json import loads, dumps
 
-# Create your tests here.
+DEFAULT_TEAM = {'pk': 1, 'name': 'InitTestTeam', 'label': '', 'boards': []}
+CORE_URL = '/api/ws100/core'
+
+
+class TeamTests(APITestCase):
+
+    def setUp(self):
+        self.initial_team = Team(name=DEFAULT_TEAM['name'], label=DEFAULT_TEAM['label'])
+        self.initial_team.save()
+
+    def test_will_return_response_with_list_of_teams(self):
+        response = self.client.get(CORE_URL + '/teams/')
+        teams = response.data['results']
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(teams), 1)
+
+    def test_team_in_response_has_attributes(self):
+        response = self.client.get(CORE_URL + '/teams/')
+        teams = response.data['results']
+        self.assertEqual(teams, [DEFAULT_TEAM])
