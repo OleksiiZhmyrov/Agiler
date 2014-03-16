63d5bd0ea5f2ffeea37b8d2c37fe99b4913d05a6
Oleksii Zhmyrov <zhmyrov@gmail.com>
2014-03-16 19:26:28 +0200

    Improve static files handling

--- Agiler/settings.py
+++ Agiler/settings.py
@@ -27,9 +27,6 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -56,10 +53,6 @@ ROOT_URLCONF = 'Agiler.urls'
 
 WSGI_APPLICATION = 'Agiler.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -74,32 +67,24 @@ TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, 'ui/templates/'),
 )
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'ui/static'),
+    os.path.join(PROJECT_ROOT, 'ui/static/'),
+    os.path.join(PROJECT_ROOT, 'static/distrib/')
 )
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
--- Agiler/urls.py
+++ Agiler/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from Agiler import settings
 
 from django.contrib import admin
 
