e14e0a78886e5a96c74b22fd2890a28d12b2c8bd
Tetiana Pozniakova <tetiana.pozniakova@gmail.com>
2014-03-22 21:56:02 +0200

    Boards menu JS

--- retro/serializers.py
+++ retro/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 from retro.models import Sticker, Board
 from model_choices import *
 from core.models import Sprint, Team
+from core.serializers import TeamSerializer
 
 
 class StickerSerializer(serializers.Serializer):
     pk = serializers.Field()
     summary = serializers.CharField(required=True, max_length=1024)
-    is_completed = serializers.BooleanField(required=False, default=False)
     type = serializers.ChoiceField(choices=STICKER_TYPE_CHOICES, default=STICKER_TYPE_GOOD)
     status = serializers.ChoiceField(choices=STICKER_STATUS_CHOICES, default=STICKER_STATUS_CREATED)
     owner = serializers.Field(source='owner.pk')
@@ -23,7 +23,7 @@ class StickerSerializer(serializers.Serializer):
 
     class Meta:
         model = Sticker
-        fields = ('pk', 'summary', 'is_completed', 'type', 'owner', 'board',)
+        fields = ('pk', 'summary', 'status', 'type', 'owner', 'board',)
 
 
 class BoardSerializer(serializers.Serializer):
@@ -33,7 +33,8 @@ class BoardSerializer(serializers.Serializer):
     owner = serializers.Field(source='owner.pk')
 
     sprint = serializers.PrimaryKeyRelatedField(many=False, queryset=Sprint.objects.all())
-    team = serializers.PrimaryKeyRelatedField(many=False, queryset=Team.objects.all())
+    team = TeamSerializer(many=False)
+
 
     stickers = StickerSerializer(many=True, required=False)
 
---
+++ ui/static/js/menu/boards.js
@@ -0,0 +1,44 @@
+$(function(){
+
+    var Board = Backbone.Model.extend({});
+
+    var BoardView = Backbone.View.extend({
+        render: function () {
+            this.el = ich.boardsList(this.model.toJSON());
+            return this;
+        }
+    });
+
+    var BoardCollection = Backbone.Collection.extend({
+        model: Board,
+        url: '/api/ws100/retro/boards/',
+
+        parse: function(response) {
+            return response.results;
+  }
+    });
+
+    var AppView = Backbone.View.extend({
+        tagName: 'li',
+        className: 'item',
+
+        initialize: function() {
+            this.boards = new BoardCollection();
+            this.boards.bind('all', this.render, this);
+            this.boards.fetch();
+        },
+
+        render: function () {
+            this.$el.empty();
+            this.boards.each(function (board) {
+                $(this.el).append(new BoardView({model: board}).render().el);
+            }, this);
+
+            return this;
+        }
+    })
+
+    var app = new AppView();
+    $('#boadrs-list').prepend(app.render().el);
+
+});
