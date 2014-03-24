5556694ebbff0db5e3c43b209f30600a2afe288e
Tetiana Pozniakova <tetiana.pozniakova@gmail.com>
2014-03-24 21:26:36 +0200

    Settings menu  -remove comments

--- ui/static/js/menu/settings.js
+++ ui/static/js/menu/settings.js
@@ -5,33 +5,19 @@ $(function(){
     });
 
     var SettingsView = Backbone.View.extend({
-                tagName: 'li',
+        tagName: 'li',
         className: 'item',
 
         initialize: function() {
             this.setting = new Settings();
-                                                //console.log(this)
-
             this.setting.bind('sync', this.render, this);
             this.setting.fetch();
-                                    //console.log(this.setting)
         },
 
         render: function () {
-            //console.log(this.setting.toJSON())
-            //this.el = ich.settingsList(this.setting.toJSON());
             $('#settings-list').html(ich.settingsList(this.setting.toJSON()));
-                        //console.log(this)
-            //return this;
         }
     });
 
     var settings = new SettingsView();
-    //console.log(settings.render())
-        //console.log(settings.render().el)
-
-//    console.log(settings.render().el);
-//    console.log($('#settings-list').append(settings.render().el));
-   //$('#settings-list').html(settings.render().el);
-
 });
