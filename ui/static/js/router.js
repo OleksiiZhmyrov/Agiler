efe2b7ffb58208e8c02f4d5473f02ff2f2abea00
Oleksii Zhmyrov <zhmyrov@gmail.com>
2014-03-29 23:06:29 +0200

    Add Teams page. Update router

--- ui/static/js/router.js
+++ ui/static/js/router.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
     var SettingsRouter = Backbone.Router.extend ({
         routes: {
             '' : 'homePage',
+            'settings/teams': 'teamsPage',
             'settings/sprints': 'sprintsPage'
         },
         homePage: function () {
             hideAllPages();
+        },
+        teamsPage: function () {
+            hideAllPages();
+            renderTeams();
         },
         sprintsPage: function () {
             hideAllPages();
---
+++ ui/static/js/settings/teams/teams.js
@@ -0,0 +1,47 @@
+var Team = Backbone.Model.extend({});
+
+var TeamView = Backbone.View.extend({
+    render: function () {
+        this.el = ich.teamsList(this.model.toJSON());
+        return this;
+    }
+});
+
+var TeamsCollection = Backbone.Collection.extend({
+    model: Team,
+    url: '/api/ws100/core/teams/',
+
+    parse: function(response) {
+        return response.results;
+    }
+});
+
+var TeamsView = Backbone.View.extend({
+    tagName: 'tbody',
+
+    initialize: function() {
+        this.boards = new TeamsCollection();
+        this.boards.bind('all', this.render, this);
+        this.boards.fetch({
+            error: (function (e) {
+                alert(' Service request failure: ' + e);
+            })
+        });
+    },
+
+    render: function () {
+        this.$el.empty();
+        this.boards.each(function (team) {
+            $(this.el).append(new TeamView({model: team}).render().el);
+        }, this);
+
+        return this;
+    }
+})
+
+var teams = new TeamsView();
+
+function renderTeams(){
+    $('#teams-list').append(teams.render().el);
+    $('#page-teams').show();
+}
