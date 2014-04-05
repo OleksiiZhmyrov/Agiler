72a66b2069194be1a93e1898316dc0d2713e1275
Oleksii Zhmyrov <zhmyrov@gmail.com>
2014-04-05 22:05:09 +0300

    Add router for board page. Modify board page template

---
+++ ui/static/js/pages/board.js
@@ -0,0 +1,8 @@
+
+function renderBoard(pk){
+    // TODO: Render page here
+    $('#board-page').show();
+
+    //TODO: Remove alert, this is only to verify that router has passed correct PK to renderer
+    alert('Page for board with pk = ' + pk);
+}
--- ui/static/js/router.js
+++ ui/static/js/router.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
     var SettingsRouter = Backbone.Router.extend ({
         routes: {
-            '' : 'homePage',
+            '': 'homePage',
+            'boards/:pk': 'boardPage',
             'settings/users': 'usersPage',
             'settings/teams': 'teamsPage',
             'settings/sprints': 'sprintsPage'
         },
         homePage: function () {
             hideAllPages();
+        },
+        boardPage: function (pk) {
+            hideAllPages();
+            renderBoard(pk);
         },
         usersPage: function () {
             hideAllPages();
