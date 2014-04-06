6774976a73f55f1b286f6b3addade46837cdb6ed
Tetiana Pozniakova <tetiana.pozniakova@gmail.com>
2014-04-06 13:29:52 +0300

    Merge serializers.py

--- retro/serializers.py
+++ retro/serializers.py
@@ -3,6 +3,7 @@ from retro.models import Sticker, Board
 from model_choices import *
 from core.models import Sprint, Team
 from core.serializers import TeamSerializer, SprintSerializer
+from retro.objects import RetroBoardContainer
 
 
 class StickerSerializer(serializers.Serializer):
@@ -46,3 +47,25 @@ class BoardSerializer(serializers.Serializer):
     class Meta:
         model = Board
         fields = ('pk', 'isActive', 'voteLimit', 'owner', 'sprint', 'team', 'stickers',)
+
+
+class RetroBoardContainerSerializer(serializers.Serializer):
+    was_good = StickerSerializer(many=True, required=False)
+    need_to_change = StickerSerializer(many=True, required=False)
+    action_point = StickerSerializer(many=True, required=False)
+    is_active = serializers.BooleanField(required=False, default=False)
+    vote_limit = serializers.IntegerField(required=False, default=3)
+    team = serializers.Field()
+    sprint = serializers.Field()
+
+    def restore_object(self, attrs, instance=None):
+        if instance is not None:
+            instance.was_good = attrs.get('was_good', instance.was_good)
+            instance.need_to_change = attrs.get('need_to_change', instance.need_to_change)
+            instance.action_point = attrs.get('action_point', instance.action_point)
+            instance.is_active = attrs.get('is_active', instance.is_active)
+            instance.vote_limit = attrs.get('vote_limit', instance.vote_limit)
+            instance.team = attrs.get('team', instance.team)
+            instance.sprint = attrs.get('sprint', instance.sprint)
+            return instance
+        return RetroBoardContainer(**attrs)
